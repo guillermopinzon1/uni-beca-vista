95601249110d4c149d75a37709dd4a5016b9015a
mariacuervoo <maria.cuervo@farmatodo.com>
2026-02-24 00:04:20 -0400

    Updates Web

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -72,7 +72,7 @@ const Home = () => {
                 className="border-2 border-white text-white bg-white/15 hover:bg-white/25 rounded-xl px-6 h-12 font-semibold shadow-lg"
                 onClick={() => navigate("/vocational")}
               >
-                <Compass className="w-5 h-5 mr-2" /> Test Vocacional
+                <Compass className="w-5 h-5 mr-2" /> Solicitud de Orientación Vocacional
               </Button>
             </div>
           </motion.div>
